6701a9d0e26f6ed83c59a24422e34eb7da4710a4
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-17 23:25:33 +0200

    Update bl-helium-postinstall.sh

--- bl-helium-postinstall.sh
+++ bl-helium-postinstall.sh
@@ -6,9 +6,8 @@ bunsen_ver="Helium"
 vb_package="virtualbox-5.2"
 ep_url="https://download.virtualbox.org/virtualbox/5.2.12/Oracle_VM_VirtualBox_Extension_Pack-5.2.12.vbox-extpack"   #https://www.virtualbox.org/wiki/Downloads
 current_dir="$(dirname "$(readlink -f "$0")")"
+[ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ] && laptop="true"
 
-[ "$(id -u)" -ne 0 ] && echo "Administrative privileges needed" && exit 1
-read -p "Are you config a laptop (y/N)? " laptop
 
 
 #=== FUNCTION ==================================================================
@@ -25,7 +24,26 @@ Usage: '$(basename $0)' [-h] [-l] [-a <actions>]
 }
 
 
+#=== FUNCTION ==================================================================
+# NAME: do_action
+# DESCRIPTION: Show question for do an action and determine if do or not
+# RETURN: 0 if should be do de action, 1 in other case
+#===============================================================================
+n=1
+function do_action() {
+	n=$((n+1))
+	[ "$actions" ] && echo "$actions" | grep -w "$n" &> /dev/null || return 1
+	q="$1"
+	echo -en "\n\e[1m[$n] \e[4m$q\e[0m$([ ! "$list" ] && echo " (Y/n)?") "
+	[ "$list" ] && return 1
+	read -p q
+	[ "${q,,}" != "n" ] && return 0
+	return 1
+}
 
+
+
+[ "$(id -u)" -ne 0 ] && echo "Administrative privileges needed" && exit 1
 while getopts ":hla:" o; do
 	case "$o" in
 	h)	help ;;
@@ -41,42 +59,35 @@ while getopts ":hla:" o; do
 done
 
 
-n=1
+
 
 ########################################################################
 #### PACKAGES ##########################################################
 ########################################################################
 apt-get update
-	
-q="Install some useful packages"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+
+# Mix packages
+if do_action "Install some useful packages"
 	apt-get install -y vim vls ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback
 	apt-get install -y haveged		# Avoid delay first login
 fi
 
-q="Install rofi launcher"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+# Rofi laucher
+if do_action "Install rofi launcher"
 	apt-get install -y rofi
 	cat "$current_dir"/config/rofi.conf >> /usr/share/bunsen/skel/.Xresources
 	ls -d /home/* | xargs -I {} cp -v /usr/share/bunsen/skel/.Xresources {}/
 	xrdb -load /usr/share/bunsen/skel/.Xresources  
 fi
 
-
 # PlayOnLinux
-q="Install PlayOnLinux"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Install PlayOnLinux"
 	apt-get install -y winbind
 	apt-get install -y playonlinux
 fi
 
 # VirtualBox
-q="Install VirtualBox and add repositories"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Install VirtualBox and add repositories"
 	echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib" > /etc/apt/sources.list.d/virtualbox.list
 	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
@@ -85,9 +96,7 @@ if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list"
 fi
 
 # VirtualBox Extension Pack
-q="Install Extension Pack"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Install Extension Pack"
 	t=$(mktemp -d)
 	wget -P "$t" "$ep_url"  
 	vboxmanage extpack install --replace "$t"/*extpack
@@ -95,9 +104,7 @@ if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list"
 fi
 
 # Sublime Text 3
-q="Install Sublime-Text 3 and add repositories"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Install Sublime-Text 3 and add repositories"
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	apt-get update
@@ -107,9 +114,7 @@ if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list"
 fi
 
 # Google Chrome
-q="Install Google Chrome and add repositories"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Install Google Chrome and add repositories"
 	echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 	apt-get update
@@ -122,36 +127,26 @@ fi
 ########################################################################
 #### FILES #############################################################
 ########################################################################
-q="Copy some cool scripts"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Copy some cool scripts"
 	chmod +x "$current_dir"/bin/*
 	cp -v "$current_dir"/bin/* /usr/bin/
 	update-notification.sh -I      # Install update-notification
 fi
 
-q="Copy some cool wallpapers"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Copy some cool wallpapers"
 	cp "$current_dir"/wallpapers/* /usr/share/images/bunsen/wallpapers
 fi
 
-q="Copy some cool icon packs"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Copy some cool icon packs"
 	zip -FF "$current_dir"/files/icons.zip --out "$current_dir"/files/icons-full.zip
 	unzip "$current_dir"/files/icons-full.zip -d /usr/share/icons/
 fi
 
-q="Copy some cool fonts"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Copy some cool fonts"
 	unzip "$current_dir"/files/fonts.zip -d /usr/share/fonts/
 fi
 
-q="Copy some cool themes"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Copy some cool themes"
 	unzip "$current_dir"/files/themes.zip -d /usr/share/themes/
 fi
 
@@ -160,34 +155,26 @@ fi
 #### SYSTEM CONFIG #####################################################
 ########################################################################
 ## DISABLE DISPLAY MANAGER
-q="Disable graphical display manager"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Disable graphical display manager"
 	systemctl set-default multi-user.target
 	sed -i "/#BL-POSTINSTALL/Id" /etc/profile
 	echo '[ $(tty) = "/dev/tty1" ] && startx; exit   #BL-POSTINSTALL' >> /etc/profile
 fi
 
-q="Enable CTRL+ALT+BACKSPACE for kill X"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Enable CTRL+ALT+BACKSPACE for kill X"
 	sed -i "/#BL-POSTINSTALL/Id" /etc/default/keyboard
 	echo 'XKBOPTIONS="terminate:ctrl_alt_bksp    #BL-POSTINSTALL"' >> /etc/default/keyboard
 fi
 
 ### SERVICES
-q="Disable some stupid services"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Disable some stupid services"
 	systemctl disable NetworkManager-wait-online.service
 	systemctl disable ModemManager.service
 	systemctl disable pppd-dns.service
 fi
 
 # GRUB CONIFG
-q="Skip Grub menu and enter Bunsen directly"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Skip Grub menu and enter Bunsen directly"
 	for i in $(cat "$current_dir"/config/grub_skip.conf  | cut -f1 -d=);do
 		sed -i "/\b$i=/Id" /etc/default/grub
 	done
@@ -195,9 +182,7 @@ if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list"
 	update-grub
 fi
 
-q="Show messages during boot"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Show messages during boot"
 	for i in $(cat "$current_dir"/config/grub_text.conf  | cut -f1 -d=);do
 		sed -i "/\b$i=/Id" /etc/default/grub
 	done
@@ -210,25 +195,19 @@ fi
 #### USER CONFIG #######################################################
 ########################################################################
 # bl-exit theme
-q="Configure bl-exit classic theme"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Configure bl-exit classic theme"
 	sed  -i "s/^theme *= *.*/theme = classic/" /etc/bl-exit/bl-exitrc	
 	sed  -i "s/^rcfile *= *.*/rcfile = none/" /etc/bl-exit/bl-exitrc
 fi
 
 # tint2 config
-q="Add tin2 themes"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Add tin2 themes"
 	cp -v"$current_dir"/config/*.tint /usr/share/bunsen/skel/.config/tint2/
 	ls -d /home/* | xargs -I {} cp -v "$current_dir"/config/*.tint {}.config/tint2/
 fi
 
 # aliases
-q="Add some aliases"
-read -p "$(echo -e "\n\e[1m[$n] \e[4m$q$([ ! "$list" ] && echo " (Y/n)?")\e[0m ")" q
-if [ ! "$actions" ] || echo "$actions" | grep -w "$n" &>/dev/null && [ ! "$list" ] && [ "${q,,}" != "n" ]; then
+if do_action "Add some aliases"
 	sed -i "/#BL-POSTINSTALL/Id" /usr/share/bunsen/skel/.bash_aliases
 	cat "$current_dir"/config/aliases >> /usr/share/bunsen/skel/.bash_aliases
 	ls -d /home/* | xargs -I {} cp -v /usr/share/bunsen/skel/.bash_aliases {}/
