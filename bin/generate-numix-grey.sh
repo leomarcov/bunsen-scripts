54e45d4ecdb279cd0d04a71979071c61e351039a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-20 20:57:18 +0200

    Update generate-numix-grey.sh

--- bin/generate-numix-grey.sh
+++ bin/generate-numix-grey.sh
@@ -4,37 +4,29 @@ for p in numix-icon-theme paper-icon-theme bunsen-paper-icon-theme; do
 	dpkg -l | egrep "ii *$p" &> /dev/null || { echo "Package $p needed"; exit; } 
 done 
 
-
-
 [ ! "$list" ] && [ "$(id -u)" -ne 0 ] && echo "Administrative privileges needed" && exit 1
-mkdir /usr/share/icons/Numix-Bunsen/
-cd /usr/share/icons/Numix-Bunsen/
-cp /usr/share/icons/Numix/index.theme /usr/share/icons/Numix-Bunsen/
-sed -i "s/^Name *= *.*/Name=Numix-Bunsen/" /usr/share/icons/Numix-Bunsen/index.theme
-sed -i "s/^Inherits *= *.*/Inherits=Numix/" /usr/share/icons/Numix-Bunsen/index.theme
-sed -i "s/^Comment *= *.*/Comment=Theme mix Numix-Paper for BunsenLabs/" /usr/share/icons/Numix-Bunsen/index.theme
 
-# GENERATE DIRS
+# GENERATE /usr/share/icons/numix-paper dirs
+mkdir /usr/share/icons/Numix-Paper/
+cd /usr/share/icons/Numix-Paper/
+cp /usr/share/icons/Numix/index.theme /usr/share/icons/Numix-Paper/
+sed -i "s/^Name *= *.*/Name=Numix-Paper/" /usr/share/icons/Numix-Paper/index.theme
+sed -i "s/^Inherits *= *.*/Inherits=Numix/" /usr/share/icons/Numix-Paper/index.theme
+sed -i "s/^Comment *= *.*/Comment=Theme mix Numix-Paper for BunsenLabs/" /usr/share/icons/Numix-Paper/index.theme
 for d in $(find ../Numix/ -type d); do
 	[ "$d" == "../Numix" ] && continue
 	mkdir -v $(echo $d | sed 's/..\/Numix\///g' ) 
 done
 
-read -p "GENERATE ALL LINKS FROM PAPER-BUNSEN" 
-for f in $(find ../Paper-Bunsen -type f); do
-	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper-Bunsen\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
-done
-
-read -p "GENERATE PANEL LINKS FROM PAPER" 
-for f in $(ls ../Paper/*/panel/*); do
-	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
-done
-
-read -p "GENERATE APP LINKS FROM PAPER"
+read -p "GENERATE PAPER LINKS"
 for f in $(ls ../Paper/*/apps/*); do
 	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
 done
 
+read -p "GENERATE PAPER-BUNSEN LINKS" 
+for f in $(find ../Paper-Bunsen -type f); do
+	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper-Bunsen\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
+done
 
 read -p "GENERATE NUMIX LINKS"
 default_color="grey"
