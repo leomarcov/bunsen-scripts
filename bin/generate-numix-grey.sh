ae54ff9d0f9faedfb8a8054db8a8b7f975897d86
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-20 21:00:54 +0200

    Update generate-numix-grey.sh

--- bin/generate-numix-grey.sh
+++ bin/generate-numix-grey.sh
@@ -7,7 +7,7 @@ done
 [ ! "$list" ] && [ "$(id -u)" -ne 0 ] && echo "Administrative privileges needed" && exit 1
 
 # GENERATE /usr/share/icons/numix-paper dirs
-mkdir /usr/share/icons/Numix-Paper/
+mkdir -v /usr/share/icons/Numix-Paper/
 cd /usr/share/icons/Numix-Paper/
 cp /usr/share/icons/Numix/index.theme /usr/share/icons/Numix-Paper/
 sed -i "s/^Name *= *.*/Name=Numix-Paper/" /usr/share/icons/Numix-Paper/index.theme
@@ -18,16 +18,19 @@ for d in $(find ../Numix/ -type d); do
 	mkdir -v $(echo $d | sed 's/..\/Numix\///g' ) 
 done
 
+echo
 read -p "GENERATE PAPER LINKS"
 for f in $(ls ../Paper/*/apps/*); do
 	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
 done
 
+echo
 read -p "GENERATE PAPER-BUNSEN LINKS" 
 for f in $(find ../Paper-Bunsen -type f); do
 	ln -sv "../../$f" $(echo "$f" | sed 's/..\/Paper-Bunsen\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
 done
 
+echo
 read -p "GENERATE NUMIX LINKS"
 default_color="grey"
 for link in $(find ../Numix/ -type l); do 
