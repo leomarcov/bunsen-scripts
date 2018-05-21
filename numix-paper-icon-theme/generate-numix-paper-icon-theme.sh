894b2766c760cd9c377e34a7e661399cad4803ca
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-21 19:59:25 +0200

    Update generate-numix-paper-icon-theme.sh

--- numix-paper-icon-theme/generate-numix-paper-icon-theme.sh
+++ numix-paper-icon-theme/generate-numix-paper-icon-theme.sh
@@ -1,51 +1,65 @@
 #!/bin/bash
 #===================================================================================
 # FILE: generate-numix-paper-icon-theme.sh.sh
-# DESCRIPTION: Generates new icon theme based on Numix (grey icons) and Paper
-# REQUIREMENTS: numix-icon-theme paper-icon-theme
+# DESCRIPTION: Generates new icon theme based on Numix (using folder grey icons) and Paper
+# REQUIREMENTS: numix-icon-theme [paper-icon-theme] [bunsen-paper-icon-theme]
 # AUTHOR: Leonardo Marco
 # VERSION: 1.0
 # CREATED: 21.05.2018
 #===================================================================================
 
 # CHECKS
-for t in Numix Paper; do
-if [ ! -d /usr/share/icons/$t ]; then
-	echo "$t icon theme not fund in /usr/share/icons/$t"
+read -p "Install directory (/usr/share/icons/Numix-Paper/): " install_dir
+[ ! "$install_dir" ] && install_dir="/usr/share/icons/Numix-Paper"
+eval install_dir="$install_dir/"
+
+numix_dir="$(dirname "$install_dir")/Numix"
+paper_dir="$(dirname "$install_dir")/Paper"
+paper-bunsen_dir="$(dirname "$install_dir")/Paper-Bunsen"
+
+if [ ! -d "numix_dir" ]; then
+	echo "Numix icon theme sould be installed in $numix_dir, but not found"
 	exit 1
 fi
-done
-[ ! "$list" ] && [ "$(id -u)" -ne 0 ] && echo "Administrative privileges needed" && exit 1
+if [ ! -d "paper_dir" ]; then
+	echo "Paper icon theme sould be installed in $paper_dir, but not found"
+	read -p "Continue using only Numix icons? (Y/n) " q
+	[ "${q,,}" = "n" ] && exit 0
+fi
 
 
 echo -e "\nGENERATING /usr/share/icons/numix-paper dirs"
-[ ! -d /usr/share/icons/Numix-Paper/ ] && mkdir -v /usr/share/icons/Numix-Paper/
-cd /usr/share/icons/Numix-Paper/
+[ ! -d "$install_dir" ] && mkdir -v "$install_dir"
+[ ! -d /"$install_dir" ] && echo "Cant create $install_dir directory" && exit 1
+
+cd "$install_dir"
 find . ! -name "$(basename $0)" -exec rm -rf {} \; 2> /dev/null
-cp /usr/share/icons/Numix/index.theme /usr/share/icons/Numix-Paper/
-sed -i "s/^Name *= *.*/Name=Numix-Paper/" /usr/share/icons/Numix-Paper/index.theme
-sed -i "s/^Inherits *= *.*/Inherits=Numix/" /usr/share/icons/Numix-Paper/index.theme
-sed -i "s/^Comment *= *.*/Comment=Theme mix Numix-Paper for BunsenLabs/" /usr/share/icons/Numix-Paper/index.theme
+cp "$numix_dir/index.theme" "$install_dir"
+sed -i "s/^Name *= *.*/Name=Numix-Paper/" "$install_dir/index.theme"
+sed -i "s/^Inherits *= *.*/Inherits=Numix/" "$install_dir/index.theme"
+sed -i "s/^Comment *= *.*/Comment=Theme mix Numix-Paper for BunsenLabs/" "$install_dir/index.theme"
 for d in $(find ../Numix/ -type d); do
 	[ "$d" == "../Numix" ] && continue
 	mkdir -v $(echo $d | sed 's/..\/Numix\///g' ) 
 done
 
-echo -e "\nºnGENERATING PAPER LINKS..."
-for f in $(ls ../Paper/*/apps/* ../Paper/*/panel/*); do
-	ln -svf "../../$f" $(echo "$f" | sed 's/..\/Paper\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
-done
-# Change terminator icon for terminal 
-for f in $(find . -name "terminator.*"); do
-	ln -svf "$(basename $(echo "$f"  | sed 's/terminator/terminal/g'))" "$f" 
-done
+if [ -d "$paper_dir" ]; then
+	echo -e "\nºnGENERATING PAPER LINKS..."
+	for f in $(ls ../Paper/*/apps/* ../Paper/*/panel/*); do
+		ln -svf "../../$f" $(echo "$f" | sed 's/..\/Paper\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
+	done
+	# Change terminator icon for terminal 
+	for f in $(find . -name "terminator.*"); do
+		ln -svf "$(basename $(echo "$f"  | sed 's/terminator/terminal/g'))" "$f" 
+	done
+fi
 
-if [ -d /usr/share/icons/Paper-Bunsen/ ]; then
-echo -e "\n\nGENERATING PAPER-BUNSEN LINKS..." 
-for f in $(find ../Paper-Bunsen -mindepth 2 -type f); do
-	echo $f | grep xfpm-ac-adapter.png &> /dev/null && continue
-	ln -svf "../../$f" $(echo "$f" | sed 's/..\/Paper-Bunsen\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
-done
+if [ -d "$paper-bunsen_dir" ]; then
+	echo -e "\n\nGENERATING PAPER-BUNSEN LINKS..." 
+	for f in $(find ../Paper-Bunsen -mindepth 2 -type f); do
+		echo $f | grep xfpm-ac-adapter.png &> /dev/null && continue
+		ln -svf "../../$f" $(echo "$f" | sed 's/..\/Paper-Bunsen\///g' | sed 's/^[0-9]\+x//g') 2> /dev/null
+	done
 fi
 
 echo -e "\n\nGENERATING NUMIX LINKS..."
