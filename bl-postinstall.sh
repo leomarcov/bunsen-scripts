147fe241a59ad6b93b06f1b37933cb5d26f742e2
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-22 15:21:50 +0200

    Update bl-postinstall.sh

--- bl-postinstall.sh
+++ bl-postinstall.sh
@@ -104,9 +104,10 @@ if do_action "INSTALL AND ADD REPOSITORIES: Sublime-Text 3"; then
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	apt-get update
-	apt-get install sublime-text
-	update-alternatives --install /usr/bin/bl-text-editor bl-text-editor /usr/bin/subl 90
-	update-alternatives --set bl-text-editor /usr/bin/subl
+	if apt-get install sublime-text; then
+		update-alternatives --install /usr/bin/bl-text-editor bl-text-editor /usr/bin/subl 90 && \
+		update-alternatives --set bl-text-editor /usr/bin/subl
+	fi
 fi
 
 # Google Chrome
@@ -123,17 +124,18 @@ if do_action "INSTALL AND ADD REPOSITORIES: VirtualBox"; then
 	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 	apt-get update
-	apt-get install -y linux-headers-$(uname -r) "$vb_package"
 	
-	# Add VirtualBox in OpenBox menu:
-	sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/'   /usr/share/bunsen/skel/.config/openbox/menu.xml
-	ls /home/*/.config/openbox/menu.xml | xargs -I {} sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/' {}
+	if apt-get install -y linux-headers-$(uname -r) "$vb_package"; then
+		# Add VirtualBox in OpenBox menu:
+		sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/'   /usr/share/bunsen/skel/.config/openbox/menu.xml
+		ls /home/*/.config/openbox/menu.xml | xargs -I {} sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/' {}
+	fi
 fi
 
 # VirtualBox Extension Pack
 if do_action "INSTALL: Extension Pack"; then
 	t=$(mktemp -d)
-	wget -P "$t" "$ep_url"  
+	wget -P "$t" "$ep_url"  && \
 	yes | vboxmanage extpack install --replace "$t"/*extpack 
 	rm -rf "$t"
 fi
