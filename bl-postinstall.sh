53dc2c40cd29a4ff299ee9295564223b646df909
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-22 13:57:08 +0200

    Update bl-postinstall.sh

--- bl-postinstall.sh
+++ bl-postinstall.sh
@@ -234,8 +234,27 @@ fi
 
 #=== SYSTEM CONFIG ACTIONS ====================================================================
 ## Disable DM
-if do_action "CONFIG: disable graphical display manager"; then
+if do_action "CONFIG: disable graphical display manager and install text locker"; then
 	systemctl set-default multi-user.target
+	apt-get remove lightdm
+
+	# Install physlock
+	apt-get install libpam0g-dev	
+	t=$(mktemp -d)
+	wget -P "$t" "https://github.com/muennich/physlock/archive/master.zip"  
+	unzip "$t"/*.zip -d "$t"
+	rm "$t"/*.zip
+	make -C "$t"/*
+	make -C "$t"/* install
+	
+	# Config physlock as default locker
+	sed  -i 's/^LOCK_COMMAND *= *.*/LOCK_COMMAND=\(\/usr\/local\/bin\/physlock\)/' /usr/bin/bl-lock
+	
+	# Config physlock for start after suspend
+	cp "$current_dir"/postinstall-files/physlock.service /etc/systemd/system/
+	systemctl enable physlock.service
+	
+	# Config tty1 to autoexec startx
 	sed -i "/#$comment_auto/Id" /etc/profile
 	echo '[ $(tty) = "/dev/tty1" ] && startx && exit   '"$comment_auto" >> /etc/profile
 fi
