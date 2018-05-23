60859791d2962ac6c5eb0d69c9b18599a7efb46a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 11:21:47 +0200

    Create 30-wallpaper.sh

---
+++ postinstall-scripts/30-wallpaper.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+# ACTION: Set default wallpaper for all users
+# DESC: Aptenodytes Forsteri Wallpaper by Nixiepro is a clear and beautiful Bunsen wallpaper.
+# DEFAULT: y
+
+basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
