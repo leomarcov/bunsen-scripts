0a31b0903c0c13ad14bf6dc3337574e1a02378a5
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 14:14:15 +0200

    Update 40_shortkey-kill-x.sh

--- postinstall-scripts/40_shortkey-kill-x.sh
+++ postinstall-scripts/40_shortkey-kill-x.sh
@@ -2,5 +2,12 @@
 # ACTION: Enable CTRL+ALT+BACKSPACE for kill X server
 # DEFAULT: y
 
-sed -i "/XKBOPTIONS/Id" /etc/default/keyboard
-echo 'XKBOPTIONS="terminate:ctrl_alt_bksp"'>> /etc/default/keyboard
+grep "terminate:ctrl_alt_bksp" /etc/default/keyboard &> /dev/null && exit
+
+if grep "XKBOPTIONS" /etc/default/keyboard &>/dev/null; then
+  sed -i "s/XKBOPTIONS=\"/XKBOPTIONS=\"terminate:ctrl_alt_bksp,/" /etc/default/keyboard  
+else
+  echo 'XKBOPTIONS="terminate:ctrl_alt_bksp"'>> /etc/default/keyboard
+fi
+
+
