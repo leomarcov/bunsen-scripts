3ba1074c7ff42b397b33a0168675f82f63b181d6
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 13:33:59 +0200

    Create 40_skip-grub-menu.sh

---
+++ postinstall-scripts/40_skip-grub-menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ACTION: Config GRUB for skip menu
+# DESC: If you are using only one OS in the computer you con skip GRUB menu for fast boot.
+# DEFAULT: y
+
+basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+for i in $(cat "$basedir"/postinstall-files/grub_skip.conf  | cut -f1 -d=);do
+	sed -i "/\b$i=/Id" /etc/default/grub
+done
+cat "$basedir"/postinstall-files/grub_skip.conf >> /etc/default/grub
+update-grub
