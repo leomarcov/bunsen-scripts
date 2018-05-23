0d975c20dd68add36ea0b24cfc177541c58f0abd
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 10:20:41 +0200

    Update 10_rofi.sh

--- postinstall-scripts/10_rofi.sh
+++ postinstall-scripts/10_rofi.sh
@@ -9,8 +9,10 @@ for d in /usr/share/bunsen/skel/.config/  /home/*/.config/; do
 	[ ! -d "$d/rofi/" ] && mkdir -p "$d/rofi/"
 	# Set defaul theme: android:notification:
 	echo '#include "/usr/share/rofi/themes/android_notification.theme"' > "$d/rofi/config"
+	
 	# Set has runas Super+F2:
 	sed -i 's/<command>gmrun<\/command>/<command>rofi -show run<\/command>/' "$d/openbox/rc.xml"
+	
 	# Set as runas in menu:
 	sed -zi 's/<command>[[:blank:]]*\n[[:blank:]]*gmrun[[:blank:]]*\n[[:blank:]]*<\/command>/<command>rofi -show run<\/command>/' "$d/openbox/menu.xml"
 done
