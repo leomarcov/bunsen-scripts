3404a64e0683f7208524f0187dcc64e9715d80f6
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 10:47:14 +0200

    Update 10_packages.sh

--- postinstall-scripts/10_packages.sh
+++ postinstall-scripts/10_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACTION: Install some useful packages
-# DESC: Install packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar haveged
+# DESC: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar haveged
 # DEFAULT: y
   
 packages="vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar"
