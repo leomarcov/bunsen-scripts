34a61ab91286d11868f7eaba4d600c44d1563184
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 13:26:47 +0200

    Update 40_text-DM.sh

--- postinstall-scripts/40_text-DM.sh
+++ postinstall-scripts/40_text-DM.sh
@@ -3,6 +3,8 @@
 # DESC: Boot and enter your password in text mode is cool. You can use tt1 login and physlock for enter your password when go back  from suspend and lock screen. 
 # DEFAULT: y
 
+comment_mark="#bl-postinstall.sh"
+
 systemctl set-default multi-user.target
 apt-get -y remove lightdm
 
@@ -23,5 +25,5 @@ cp "$current_dir"/postinstall-files/physlock.service /etc/systemd/system/
 systemctl enable physlock.service
 	
 # Config tty1 to autoexec startx
-sed -i '/#bl-postinstall.sh/Id' /etc/profile
-echo '[ $(tty) = "/dev/tty1" ] && startx && exit   '"$comment_auto" >> /etc/profile
+sed -i "/$comment_mark/Id" /etc/profile
+echo '[ $(tty) = "/dev/tty1" ] && startx && exit   '"$comment_mark" >> /etc/profile
