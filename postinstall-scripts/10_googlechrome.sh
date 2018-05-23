cdb10ead588490d6d9c34c94a69fdb8efb9e7b46
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 10:13:22 +0200

    Update 10_googlechrome.sh

--- postinstall-scripts/10_googlechrome.sh
+++ postinstall-scripts/10_googlechrome.sh
@@ -3,10 +3,14 @@
 # DESC: Install Google chrome, config official repositories and set as default editor
 # DEFAULT: y
 
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+# Install repositories
+grep -R "dl.google.com" /etc/apt/ &> /dev/null || echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+
+# Install package
 apt-get update
 apt-get install -y google-chrome-stable
 
+# Set as default
 update-alternatives --set x-www-browser /usr/bin/google-chrome-stable
 update-alternatives --set gnome-www-browser /usr/bin/google-chrome-stable
