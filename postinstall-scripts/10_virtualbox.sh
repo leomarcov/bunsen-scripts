d40e1e069ecfa9dcd98fae9ecc0aca1434bc8f10
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 09:29:31 +0200

    Create 10_virtualbox.sh

---
+++ postinstall-scripts/10_virtualbox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ACTION: Install VirtualBox
+# DESC: Install Virtualbox 5.2 and config official repositories
+# DEFAULT: y
+
+vb_package="virtualbox-5.2"
+
+echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib" > /etc/apt/sources.list.d/virtualbox.list
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+apt-get update
+	
+if apt-get install -y linux-headers-$(uname -r) "$vb_package"; then
+	# Add VirtualBox in OpenBox menu:
+	for f in /usr/share/bunsen/skel/.config/openbox/menu.xml  /home/*/.config/openbox/menu.xml ; do
+		! grep '<command>virtualbox<\/command>' "$f" &> /dev/null && sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/' "$f"
+	done
+fi
