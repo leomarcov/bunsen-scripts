521717dcf73ddce1405fe24c5f1c290c05da5070
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 09:31:54 +0200

    Update 10_extensionpack.sh

--- postinstall-scripts/10_extensionpack.sh
+++ postinstall-scripts/10_extensionpack.sh
@@ -2,6 +2,8 @@
 # ACTION: Install VirtualBox Extension Pack
 # DEFAULT: y
 
+ep_url="https://download.virtualbox.org/virtualbox/5.2.12/Oracle_VM_VirtualBox_Extension_Pack-5.2.12.vbox-extpack"   #https://www.virtualbox.org/wiki/Downloads
+
 t=$(mktemp -d)
 wget -P "$t" "$ep_url"  && \
 yes | vboxmanage extpack install --replace "$t"/*extpack 
