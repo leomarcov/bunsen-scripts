455a7cd88528066c2a49eda337733c2bd3d58955
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 13:39:17 +0200

    Create 40_show-boot-messages.sh

---
+++ postinstall-scripts/40_show-boot-messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ACTION: Config GRUB for show messages during boot
+# DESC: In boot process the system can show a stupid logo or messages about the booting process.
+# DEFAULT: y
+
+basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+for i in $(cat "$basedir"/postinstall-files/grub_textboot.conf  | cut -f1 -d=);do
+	sed -i "/\b$i=/Id" /etc/default/grub
+done
+cat "$basedir"/postinstall-files/grub_textboot.conf >> /etc/default/grub
+update-grub
