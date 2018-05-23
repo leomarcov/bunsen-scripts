875653ba75c7059d5e461225c8a5a4bfb46bfa5c
leomarcov <leomarcov2@gmail.com>
2018-05-24 00:20:55 +0200

    from local

--- postinstall-scripts/10_playonlinux.sh
+++ postinstall-scripts/10_playonlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACTION: Install playonlinux and some dependencies
-# INFO: PlayonLinux us graphical front-end for the Wine for run Windows apps
+# INFO: PlayonLinux is graphical front-end for the Wine for run Windows apps
 # DEFAULT: y
 
 apt-get install -y winbind
