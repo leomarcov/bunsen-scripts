0f0ef1a2f285f752b125594fb74b919dbb2ab12a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 10:11:20 +0200

    Update 10_extensionpack.sh

--- postinstall-scripts/10_extensionpack.sh
+++ postinstall-scripts/10_extensionpack.sh
@@ -5,6 +5,11 @@
 # https://www.virtualbox.org/wiki/Downloads
 ep_url="https://download.virtualbox.org/virtualbox/5.2.12/Oracle_VM_VirtualBox_Extension_Pack-5.2.12.vbox-extpack"   
 
+if ! which vboxmanage &> /dev/null; then
+  echo "$(basename $0): VirtualBox is not installed"
+  exit 1
+fi
+
 t=$(mktemp -d)
 wget -P "$t" "$ep_url"  
 [ $? -eq 0 ] && yes | vboxmanage extpack install --replace "$t"/*extpack 
