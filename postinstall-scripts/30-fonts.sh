d9a8a0993dde83f95da0ede849d9dd989247683c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 11:27:03 +0200

    Create 30-fonts.sh

---
+++ postinstall-scripts/30-fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ACTION: Install Numix-Paper icon theme and set as default for all users
+# DESC: Numix-Paper is a icon theme based on Numix and Paper icon themes.
+# DEFAULT: y
+
+basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
