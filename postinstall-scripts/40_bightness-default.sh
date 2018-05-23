22c334e9e1dc99102b7e6bf62e2b88401a184d02
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 16:09:31 +0200

    Create 40_bightness-default.sh

---
+++ postinstall-scripts/40_bightness-default.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ACTION: Set default brightness when start Openbox
+# INFO: Default brightness can be configured in brightness.sh
+# DEFAULT: y
+
+basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+! which brightness.sh &> /dev/null && bash "$basedir/postinstall-scripts/*script-brightness.sh"
+
+for f in  /usr/share/bunsen/skel/.config/openbox/autostart  /home/*/.config/openbox/autostart; do
+		sed -i "/brightness.sh -def/Id" "$d/.bashrc" 2> /dev/null
+		echo "brightness.sh -def &" >> "$f"
+done
