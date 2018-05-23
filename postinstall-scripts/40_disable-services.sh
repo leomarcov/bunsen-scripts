177dd4e9e773bdabd0ee20cc2d77b49df82f35a0
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 13:31:08 +0200

    Create 40_disable-services.sh

---
+++ postinstall-scripts/40_disable-services.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ACTION: Disable some stupid services
+# DESC: For most uses stupid services are: NetworkManager-wait-online.service, ModemManager.service, pppd-dns.service
+# DEFAULT: y
+
+systemctl disable NetworkManager-wait-online.service
+systemctl disable ModemManager.service
+systemctl disable pppd-dns.service
