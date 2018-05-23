33041ccf0d70aa2b7f8545727f433cb950e1b479
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-23 14:30:07 +0200

    Update 20_script-update-notification.sh

--- postinstall-scripts/20_script-update-notification.sh
+++ postinstall-scripts/20_script-update-notification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACTION: Install script update-notification.sh
-# DESC: Script update-notification.sh checks periodically APT updates and show in tint2 bar
+# INFO: Script update-notification.sh checks periodically APT updates and show in tint2 bar
 # DEFAULT: y
 
 basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
