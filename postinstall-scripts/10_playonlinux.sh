89065258226b5b57367d454a7d8ce0b089eee4d6
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-24 02:46:46 +0200

    Update 10_playonlinux.sh

--- postinstall-scripts/10_playonlinux.sh
+++ postinstall-scripts/10_playonlinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ACTION: Install playonlinux and some dependencies
 # INFO: PlayonLinux is graphical front-end for the Wine for run Windows apps
-# DEFAULT: y
+# DEFAULT: n
 
 apt-get install -y winbind
 apt-get install -y playonlinux
