a929aaae25ab268d3067d00233be363d50fe8636
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-24 03:27:52 +0200

    Update 20_script-autosnap.sh

--- postinstall-scripts/20_script-autosnap.sh
+++ postinstall-scripts/20_script-autosnap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ACTION: Install script autosnap.sh (need Openbox config for autosnap with center clic in titlebar)
+# ACTION: Install script autosnap.sh for autosnap windows with center click in titlebar
 # INFO: Script autosnap.sh half-maximize windows in Openbox WM.
 # DEFAULT: y
 
@@ -7,3 +7,8 @@ base_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 cp -v "$base_dir/autosnap-openbox/autosnap.sh" /usr/bin/
 chmod +x /usr/bin/autosnap.sh
+
+for d in /usr/share/bunsen/skel/.config/  /home/*/.config/; do
+  # Copy rc.xml config with shortkeys for autosnap
+	cp -v "$base_dir/postinstall-files/rc.xml" "$d/openbox/"
+done
