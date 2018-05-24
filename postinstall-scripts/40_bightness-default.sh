7257e85e3622ccd45a41642c69edfc5b755faaec
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-24 02:40:14 +0200

    Update 40_bightness-default.sh

--- postinstall-scripts/40_bightness-default.sh
+++ postinstall-scripts/40_bightness-default.sh
@@ -3,6 +3,14 @@
 # INFO: Default brightness can be configured in brightness.sh
 # DEFAULT: y
 
+
+# Check if laptop:
+[ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ] && laptop="true"
+if [ ! "$laptop" ]; then
+	echo "ERROR: computer dont seem to be a laptop"
+	exit 1
+fi
+
 base_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 ! which brightness.sh &> /dev/null && bash "$base_dir/postinstall-scripts/*script-brightness.sh"
