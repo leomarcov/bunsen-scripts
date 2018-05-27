3af9eed50066e976ffea83a0023afaf959299b9d
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-27 03:01:25 +0200

    Update 30_wallpaper.sh

--- postinstall-scripts/30_wallpaper.sh
+++ postinstall-scripts/30_wallpaper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# ACTION: Set Aptenodytes wallpaper as default
+# ACTION: Copy wallpapers pack and set Aptenodytes wallpaper as default
 # INFO: Aptenodytes Forsteri Wallpaper by Nixiepro is a clear and beautiful Bunsen wallpaper.
 # DEFAULT: y
 
 base_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 wp_default="bl-colorful-aptenodytes-forsteri-by-nixiepro.png"
 
-cp -v "$base_dir/postinstall-files/wallpapers/$wp_default" /usr/share/images/bunsen/wallpapers/
+cp -v "$base_dir/postinstall-files/wallpapers/"* /usr/share/images/bunsen/wallpapers/
 
 for f in  /usr/share/bunsen/skel/.config/nitrogen/bg-saved.cfg  /home/*/.config/nitrogen/bg-saved.cfg; do
 	sed -i 's/^file *= *.*/file='$(echo "/usr/share/images/bunsen/wallpapers/$wp_default" | sed 's/\//\\\//g' )'/' "$f"
