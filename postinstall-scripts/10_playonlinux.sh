54fdd15fc4b1dad01c6365a2da0e2068674f9892
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-05-27 02:24:27 +0200

    Update 10_playonlinux.sh

--- postinstall-scripts/10_playonlinux.sh
+++ postinstall-scripts/10_playonlinux.sh
@@ -3,5 +3,13 @@
 # INFO: PlayonLinux is graphical front-end for the Wine for run Windows apps
 # DEFAULT: n
 
-apt-get install -y winbind
+# Add i386
+sudo dpkg --add-architecture i386 && sudo apt-get update
+
+# Install some wine goodies (fonts-wine may wine about dep conflicts, exclude it if it does)
+apt-get install -y wine wine32 wine64 libwine libwine:i386 fonts-wine
+apt-get install -y wine-bin:i386 winbind
 apt-get install -y playonlinux
+
+# Install print libs for printing from Office
+sudo apt install libcups2:i386
