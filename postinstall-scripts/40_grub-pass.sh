6e78afda3cbece3f0b6ba76319c450343dcf4128
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-04 23:29:48 +0200

    Update 40_grub-pass.sh

--- postinstall-scripts/40_grub-pass.sh
+++ postinstall-scripts/40_grub-pass.sh
@@ -2,13 +2,24 @@
 # ACTION: Protect GRUB with password for prevent users edit entries
 # DEFAULT: y
 
+comment_mark="#bl-postinstall.sh"
+
+# Ask for password
 read -p "Enter password: " pass
 if [ ! "$pass" ]; then
 	echo "Password can't be empty"
 	exit 1
 fi
 
+# Config admin user and password
 pbkdf2_pass="$(echo -e "$pass\n$pass"| grub-mkpasswd-pbkdf2  | grep "grub.pbkdf2.*" -o)"
+echo 'set superusers="admin"    '"$comment_mark"'
+password_pbkdf2 admin '"$pbkdf2_pass   $comment_mark" | tee -a /etc/grub.d/40_custom 
 
-echo 'set superusers="admin"
-password_pbkdf2 admin '"$pbkdf2_pass" | tee -a /etc/default/grub
+# Config others users for select entry
+for f in /etc/grub.d/*; do 
+	sed -i 's/\bmenuentry\b/menuentry --unrestricted /g' "$i" 
+	sed -i '/echo[[:blank:]]*"[[:blank:]]*submenu/ s/submenu/submenu --unrestricted/g' $f
+done
+
+update-grub
