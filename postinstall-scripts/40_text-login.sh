b9aabe9e2fb7f49c5b24c55e9eedfb8378329a56
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-05 14:47:38 +0200

    Update 40_text-login.sh

--- postinstall-scripts/40_text-login.sh
+++ postinstall-scripts/40_text-login.sh
@@ -36,7 +36,7 @@ if which neofetch &>/dev/null; then
 	[ ! -d "/etc/systemd/system/getty@.service.d/" ] && mkdir -vp "/etc/systemd/system/getty@.service.d/"
 	[ ! -d "/usr/share/neofetch/" ] && mkdir -vp "/usr/share/neofetch/"
 	cp -v "$base_dir/neofetch-tty-login/config_tty" /usr/share/neofetch/
-	cp -v "$base_dir/neofetch-tty-login/neofetch-issue.sh" /usr/bin/
+	cp -v "$base_dir/neofetch-tty-login/neofetch_issue.sh" /usr/bin/
 	chmod -v a+x /usr/bin/neofetch-issue.sh
 	echo '[Service]
 ExecStartPre=-/bin/bash -c "/usr/bin/neofetch-issue.sh"' | tee "/etc/systemd/system/getty@.service.d/override.conf"
