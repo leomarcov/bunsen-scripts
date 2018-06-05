1d4ec38083e14cae0f006c210629006e48e52e28
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-05 05:26:43 +0200

    Update issue.sh

--- postinstall-files/issue.sh
+++ postinstall-files/issue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear > /etc/issue
 neofetch --config /usr/share/neofetch/config >> /etc/issue
-echo -e "\e[1mUsers\e[0m: $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | sed -z 's/\n/, /g' | sed 's/. $//')" >> /etc/issue
+echo -e "\e[1mUsers\e[0m: $(for u in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd); do if grep -Po '^sudo.+:\K.*$' /etc/group | grep -w "$u" &>/dev/null; then echo -en "\e[1m$u\e[0m  "; else echo -en "$u  "; fi; done)\n" >> /etc/issue
