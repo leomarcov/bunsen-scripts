4ac8b59bf20c86d59745026379e461bd79fbc728
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-05 14:21:10 +0200

    Update issue.sh

--- postinstall-files/issue.sh
+++ postinstall-files/issue.sh
@@ -2,20 +2,19 @@
 clear > /etc/issue
 echo -e "\e[90m\l\e[0m">> /etc/issue
 neofetch --config /usr/share/neofetch/config_tty >> /etc/issue
+sed -i -z 's/\n\n\n/ /g'   /etc/issue 	# Sometimes neofetch add extra \n
 
 # Pending updates
 updates=$(wc -l /var/cache/update-notification 2>/dev/null | cut -f1 -d" ")
 [ "$updates" -gt 0 ] &>/dev/null && sed -i "/Packages/ s/$/($updates pending updates)/" /etc/issue
 
-# Local IP
-ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
-[ ! "$ip" ] && ip="unassigned"
-sed -i 's/Users/Local IP/' /etc/issue
-sed -i "/Local IP/ s/:.*/: $ip/" /etc/issue
+# Add iface to local ip
+iface=$(ip route get 8.8.8.8 | awk '{print $5}')
+sed -i "/Local IP/ s/$/($iface)/" /etc/issue
 
 # Show users:
 echo -en "\e[1mUsers\e[0m: " >> /etc/issue
-for u in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd); do 
+for u in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | tac); do 
 	# Bold if sudo user:
 	grep -Po '^sudo.+:\K.*$' /etc/group | grep -w "$u" &>/dev/null && echo -en "\e[1m" >> /etc/issue	
 	# Red if lock user:
@@ -23,4 +22,5 @@ for u in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd); do
 
 	echo -en "$u\e[0m  " >> /etc/issue
 done
+echo >> /etc/issue 
 echo >> /etc/issue 
