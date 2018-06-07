5595ac7f2b8fd78c7dd6b38f68489f0bf1edab4b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-07 02:48:23 +0200

    Update poweroff_last.sh

--- other_scripts/poweroff_last.sh
+++ other_scripts/poweroff_last.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #===================================================================================
-# Shutdown machine if nobody is logged since minutes in firs parameter (or defined in script)
+# Shutdown machine if userr logged now and in last minutes in first parameter
 # AUTHOR: Leonardo Marco
 # CREATED: 2018.06.07
 #===================================================================================
 
-default_mins=20  # Wait mins if $1 is empty
+install_path="/usr/bin/poweroff_last.sh"     # Installation dir
+
 
 function try_poweroff() {
   mins="$1"
-  ! [ "$1" -eq "$1" ] &>/dev/null && mins="$default_mins"
   
   # Exit if someone is logged
   [ "$(who | wc -l)" -gt 0 ] && exit 
@@ -19,23 +19,43 @@ function try_poweroff() {
 
   # Poweroff the machine
   shutdown -h now
+
+  exit
 }
+
 
 function install() {
+  mins="$1"
+  [ "$(id -u)" -ne 0 ] && echo "Install must be run as root" && exit 1
+  [ "$install_path" = "$(readlink -f $0)" ] || sudo cp -v "$0" "$install_path"
+  chmod +x "$install_path"
 
+  (crontab -l 2>/dev/null; echo "*/$mins * * * *   $install_path $mins   #$(basename $0)") | crontab - 
+
+  exit 
 }
+
+function uninstall() {
+  rm -v "$install_path"
+  crontab -l | grep -v "#$(basename $0)" | crontab -
+
+  exit 
+}
+
 
 function help() {
 	echo -e 'Shutdown machine if nobody is logged since minutes in first parameter.
-Usage: '$(basename $0)' [min] | [-I]
-   \e[1m-I\e[0m\tInstall the script (copy and program cron every '$default_mins'minutes
-   \e[1mmin\e[0m\tMinutes for wait until try poweroff (default '$default_mins')
+Usage: '$(basename $0)' mins | -I mins
+   \e[1mmins\e[0m     Try shutdown if no users logged now and in last \e[1mmins\e[0m minutes
+   \e[1m-I mins\e[0m  Install the script (copy and program cron every \e[1mmins\e[0m minutes
+   \e[1m-U mins\e[0m  Uninstall the script
 '
-	exit 0
+	exit
 }
 
-case "$1" in
-  -I)         install       ;;
-  ""|[0-9]+)  try_poweroff  ;;
-  *)          help          ;;
-esac
+
+# ACTIONS
+[ "$1" = "-U" ] && uninstall
+[ "$1" = "-I" ] && [ "$2" -eq "$2" ] &>/dev/null && install "$2"
+[ "$1" -eq "$1" ] && try_poweroff "$1"
+help
