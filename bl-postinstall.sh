d11195ac304a074437f67c0c5387d0b05ad8d384
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-06-17 18:56:54 +0200

    Update bl-postinstall.sh

--- bl-postinstall.sh
+++ bl-postinstall.sh
@@ -97,7 +97,7 @@ scripts_dir="$base_dir/postinstall-scripts/"
 n=0
 
 # For each script in ./postinstall-scripts
-for script in "$scripts_dir"/*; do
+for script in "$scripts_dir"/[0-9]*; do
 	head="$(head -10 "$script")"
 	# Get ACTION field:
 	action="$(echo "$head" | grep "#[[:blank:]]*ACTION:" | sed 's/#[[:blank:]]*ACTION:[[:blank:]]*//')"
