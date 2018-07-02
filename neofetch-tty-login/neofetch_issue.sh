540a336007ee847e98c4cb5bffb2f70d2f0fcc33
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-07-02 19:59:37 +0200

    Update neofetch_issue.sh

--- neofetch-tty-login/neofetch_issue.sh
+++ neofetch-tty-login/neofetch_issue.sh
@@ -10,30 +10,31 @@
 # CREATED: 2018.06.05
 #===================================================================================
 
+issue="$(clear)"
+issue="${issue}\e[90m\l\e[0m"
+issue="${issue}\n$(neofetch --config /usr/share/neofetch/config_tty)"
 
-clear > /etc/issue
-echo -e "\e[90m\l\e[0m">> /etc/issue
-neofetch --config /usr/share/neofetch/config_tty >> /etc/issue
-sed -i -z 's/\n\n\n/ /g'   /etc/issue 	# Sometimes neofetch add extra \n
+issue="$(echo -e "$issue" | sed -z 's/\n\n\n/ /g'   /etc/issue)" 	# Sometimes neofetch add extra \n
 
 # Pending updates
 updates=$(wc -l /var/cache/update-notification 2>/dev/null | cut -f1 -d" ")
-[ "$updates" -gt 0 ] &>/dev/null && sed -i "/Packages/ s/$/($updates pending updates)/" /etc/issue
+[ "$updates" -gt 0 ] &>/dev/null && issue="$(echo -e "$issue" | sed "/Packages/ s/$/($updates pending updates)/")"
 
 # Add iface to local ip
 iface=$(ip route get 8.8.8.8 | awk '{print $5}')
-sed -i "/Local IP/ s/$/($iface)/" /etc/issue
+issue="$(echo -e "$issue" | sed "/Local IP/ s/$/($iface)/")"
 
 # Show users:
-echo -en "\e[1mUsers\e[0m: " >> /etc/issue
+issue="${issue}\e[1mUsers\e[0m: "
 for u in $(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | tac); do 
 	# Red if lock user:
-	[ "$(passwd -S $u | cut -f2 -d" ")" = "L" ] && echo -en "\e[91m" >> /etc/issue
+	[ "$(passwd -S $u | cut -f2 -d" ")" = "L" ] && issue="${issue}\e[91m"
 	# Add * to sudo users:
 	grep -Po '^sudo.+:\K.*$' /etc/group | grep -w "$u" &>/dev/null && u="${u}*"
 	
-	echo -en "$u\e[0m  " >> /etc/issue
+	issue="${issue}$u\e[0m  " 
 done
-echo >> /etc/issue 
-echo >> /etc/issue 
+issue="${issue}\n\n"
+
+echo "$issue" > /etc/issue
 
