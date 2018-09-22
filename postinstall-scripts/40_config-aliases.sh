59ba75ed826ad6035dcf38ad8255d9ee91484fca
leomarcov <32820131+leomarcov@users.noreply.github.com>
2018-09-22 12:04:53 +0200

    Update 40_config-aliases.sh

--- postinstall-scripts/40_config-aliases.sh
+++ postinstall-scripts/40_config-aliases.sh
@@ -9,3 +9,9 @@ for d in /home/*  /usr/share/bunsen/skel/  /root; do
 		sed -i "/$comment_mark/Id" "$d/.bash_aliases" 2> /dev/null
 		cat "$base_dir/postinstall-files/.bash_aliases" >> "$d/.bash_aliases"
 done
+
+grep -q ".bash_aliases" /root/.bashrc || echo '
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+' >> /root/.bashrc
