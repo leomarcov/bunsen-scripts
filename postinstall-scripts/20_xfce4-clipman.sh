57bf197b5138b6ee63f690002746edde81d34648
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 04:00:16 +0200

    Update 20_xfce4-clipman.sh

--- postinstall-scripts/20_xfce4-clipman.sh
+++ postinstall-scripts/20_xfce4-clipman.sh
@@ -2,7 +2,7 @@
 # ACTION: Replace clipit for xfce4-clipman (allow screenshot to clipboard)
 # DEFAULT: y
 
-apt-get install xfce4-clipman
+apt-get -y install xfce4-clipman
 apt-get remove clipit
 
 for f in /usr/share/bunsen/skel/.config/openbox/autostart  /home/*/.config/openbox/autostart; do
