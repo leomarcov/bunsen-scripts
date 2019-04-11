3aa7faebae2a8f7fb805ad71d75a9c4322e211d1
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 03:05:29 +0200

    Update 10_packages.sh

--- postinstall-scripts/10_packages.sh
+++ postinstall-scripts/10_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ACTION: Install some useful packages
-# INFO: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar haveged
+# INFO: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl
 # DEFAULT: y
   
-apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute
+apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl
 # apt-get install -y haveged  # Avoid delay first login in Helium
   
