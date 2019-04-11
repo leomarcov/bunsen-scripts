296cd9dd8816d4d8b657a9b89cc926e19e1a8896
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 16:15:00 +0200

    Update 10_packages.sh

--- postinstall-scripts/10_packages.sh
+++ postinstall-scripts/10_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACTION: Install some useful packages
-# INFO: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl
+# INFO: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl netcat
 # DEFAULT: y
   
 apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl
