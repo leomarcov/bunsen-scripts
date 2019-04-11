d4da62ba3cb3e51d1c748e52692ec590bb240af8
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 20:56:53 +0200

    Update 40_tty-login.sh

--- postinstall-scripts/40_tty-login.sh
+++ postinstall-scripts/40_tty-login.sh
@@ -10,16 +10,10 @@ systemctl set-default multi-user.target
 #apt-get -y remove lightdm
 
 # Install physlock
-apt-get install libpam0g-dev	
-t=$(mktemp -d)
-wget -P "$t" "https://github.com/muennich/physlock/archive/master.zip"  
-unzip "$t"/*.zip -d "$t"
-rm "$t"/*.zip
-make -C "$t"/*
-make -C "$t"/* install
+apt-get -y install physlock
 	
 # Config physlock as default locker
-sed  -i 's/^LOCK_COMMAND *= *.*/LOCK_COMMAND=\(\/usr\/local\/bin\/physlock\)/' /usr/bin/bl-lock
+sed  -i 's/^LOCK_COMMAND *= *.*/LOCK_COMMAND=\(\/usr\/bin\/physlock\)/' /usr/bin/bl-lock
 	
 # Config physlock for start after suspend
 cp "$base_dir"/postinstall-files/physlock.service /etc/systemd/system/
