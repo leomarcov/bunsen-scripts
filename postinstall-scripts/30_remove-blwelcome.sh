a12c4f6edf341722aa07ccfba2fc0d26294c38fe
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 04:01:28 +0200

    Update 30_remove-blwelcome.sh

--- postinstall-scripts/30_remove-blwelcome.sh
+++ postinstall-scripts/30_remove-blwelcome.sh
@@ -2,7 +2,7 @@
 # ACTION: Remove bunsen-welcome autostart script 
 # DEFAULT: y
 
-apt-get remove bunsen-welcome
+apt-get -y remove bunsen-welcome
 
 for f in /usr/share/bunsen/skel/.config/openbox/autostart  /home/*/.config/openbox/autostart; do
     sed -i "/bl-welcome/ s/^#*/#/" "$f"
