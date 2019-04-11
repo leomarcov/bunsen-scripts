a226b217f6d809df32b69e98047ac3e48f8d7935
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-11 02:53:36 +0200

    Update 30_conky-theme.sh

--- postinstall-scripts/30_conky-theme.sh
+++ postinstall-scripts/30_conky-theme.sh
@@ -7,3 +7,9 @@ base_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 for f in  /usr/share/bunsen/skel/.conkyrc  /home/*/.conkyrc ; do
 	cp -v "$base_dir"/postinstall-files/.conkyrc "$f"
 done
+
+if mount | grep -q "on /home[[:blank:]]"; then
+for f in  /usr/share/bunsen/skel/.conkyrc  /home/*/.conkyrc; do
+	sed -i '/Home usage/s/^#//g' "$f"
+done
+fi
