e2e33d3a591582355adaf9fa90bd33f39adbe0ff
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-18 16:58:10 +0200

    Update install_sublimetext.sh

--- install_sublimetext/install_sublimetext.sh
+++ install_sublimetext/install_sublimetext.sh
@@ -8,9 +8,9 @@
 # Install repositories and update
 if ! grep -R "download.sublimetext.com" /etc/apt/ &> /dev/null; then
 	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 	apt-get update
 fi
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 
 
 # Install package
