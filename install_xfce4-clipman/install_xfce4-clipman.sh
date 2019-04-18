d6a503dc450f006ca9f90c0d4ce81b83494fefbf
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-18 17:01:14 +0200

    Update install_xfce4-clipman.sh

--- install_xfce4-clipman/install_xfce4-clipman.sh
+++ install_xfce4-clipman/install_xfce4-clipman.sh
@@ -2,6 +2,9 @@
 # ACTION: Replace clipit for xfce4-clipman (allow screenshot to clipboard)
 # DEFAULT: y
 
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get -y install xfce4-clipman
 apt-get remove clipit
