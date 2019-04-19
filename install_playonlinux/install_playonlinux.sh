47058c813906ae093dbb1b96f730544fbb2b9768
leomarcov <32820131+leomarcov@users.noreply.github.com>
2019-04-19 11:01:47 +0200

    Update install_playonlinux.sh

--- install_playonlinux/install_playonlinux.sh
+++ install_playonlinux/install_playonlinux.sh
@@ -11,6 +11,7 @@ dpkg --add-architecture i386
 apt-get update
 
 # Install some wine goodies (fonts-wine may wine about dep conflicts, exclude it if it does)
+apt-get remove -y libwine
 apt-get install -y wine wine32 wine64 libwine libwine:i386 fonts-wine
 apt-get install -y wine-bin:i386 
 apt-get install -y winbind
