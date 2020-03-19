486e35f2a34240c5e8df32c2e04f05ee4c4f9bcd
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-19 13:43:11 +0100

    Update install_virtualbox.sh

--- install_virtualbox/install_virtualbox.sh
+++ install_virtualbox/install_virtualbox.sh
@@ -6,8 +6,8 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 
-vb_package="virtualbox-6.0"
-ep_url="https://download.virtualbox.org/virtualbox/6.0.4/Oracle_VM_VirtualBox_Extension_Pack-6.0.4.vbox-extpack"  
+vb_package="virtualbox-6.1"
+ep_url="https://download.virtualbox.org/virtualbox/6.1.4/Oracle_VM_VirtualBox_Extension_Pack-6.1.4.vbox-extpack"
 main_distro="$(cat /etc/apt/sources.list | grep ^deb | awk '{print $3}' | head -1)"
 
 # Install repositories and update
